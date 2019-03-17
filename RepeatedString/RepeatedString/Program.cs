3deb15cbbf6ebefb2cb22c792a27fd9a2afdc97f
Carlos Rivera <carisua@verizon.net>
2019-03-16 21:15:08 -0400

    Solution

--- RepeatedString/RepeatedString/Program.cs
+++ RepeatedString/RepeatedString/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Linq;
 
 namespace RepeatedString
 {
-    class MainClass
+    class Solution
     {
+        // Complete the repeatedString function below.
+        static long repeatedString(string s, long n)
+        {
+            int strlength = s.Length;
+            long mod = n % strlength;
+            long values = s.ToArray().Where(i => i == 'a').Count();
+
+            if (values == 0)
+                return 0;
+
+            if (strlength == n)
+                return values;
+
+            if (mod == 0)
+            {
+                long splits = n / (long)strlength;
+                return splits * values;
+            }
+            else
+            {
+                long extra = s.Substring(0, (int)mod).ToArray().Where(i => i == 'a').LongCount();
+                long splits = (n / (long)strlength);
+                return (splits * values) + extra;
+            }
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string s = "cac"; //Console.ReadLine();
+
+            long n = Convert.ToInt64(19);//Console.ReadLine());
+
+            long result = repeatedString(s, n);
+
+            Console.WriteLine(result);
         }
     }
 }
